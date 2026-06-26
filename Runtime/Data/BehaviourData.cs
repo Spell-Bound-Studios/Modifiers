71df1c48b0b72e978f016fc7072a3957b2c34917
Judsin <judsin1349@gmail.com>
2026-06-25 19:55:17 -0500

    behaviour data helpers added

--- Runtime/Data/BehaviourData.cs
+++ Runtime/Data/BehaviourData.cs
@@ -2,12 +2,12 @@
 
 using System;
 using System.Collections.Generic;
+using System.Text;
 using Spellbound.Core;
 using Spellbound.Core.ObjectData;
 using Spellbound.Core.ObjectHandling;
 using Spellbound.Core.Objects;
 using Spellbound.Core.Packing;
-using UnityEngine;
 
 namespace Spellbound.Modifiers {
     /// <summary>
@@ -19,27 +19,109 @@ namespace Spellbound.Modifiers {
         public List<StatAndValue> statModifiers; // Affix and Traits
         // Buffs
         // Debuffs
-        
-        
-        
-        
-        
-        public void Pack(ref Span<byte> buffer) => throw new NotImplementedException();
 
-        public void Unpack(ref ReadOnlySpan<byte> buffer) => throw new NotImplementedException();
-        public uint Hash { get; }
-        public ISmartPacker CreateNewInstance() => throw new NotImplementedException();
+        #region IPackerObjectData
 
-        public IPackerObjectData GetEmptyData() => throw new NotImplementedException();
+        public IPackerObjectData GetEmptyData() => new BehaviourData();
 
         public void InvokeChangeCallback(
-            byte context, ObjectParent parent, int instanceIndex, ObjectPreset preset, int surfaceIndex,
-            TransformData transformData) =>
-                throw new NotImplementedException();
+            byte context, ObjectParent parent, int instanceIndex,
+            ObjectPreset preset, int surfaceIndex, TransformData transformData) =>
+                this.ChangeCallback(context, parent, instanceIndex, preset, surfaceIndex, transformData);
 
         public void InvokeResolveCallback(
-            byte context, ObjectParent parent, int instanceIndex, ObjectPreset preset, int surfaceIndex,
-            TransformData transformData) =>
-                throw new NotImplementedException();
+            byte context, ObjectParent parent, int instanceIndex,
+            ObjectPreset preset, int surfaceIndex, TransformData transformData) =>
+                this.ResolveCallback(context, parent, instanceIndex, preset, surfaceIndex, transformData);
+
+        #endregion IPackerObjectData
+        
+        
+        #region IPacker
+
+        public void Pack(ref Span<byte> buffer) {
+            Packer.PackList(ref buffer, resourceData);
+            Packer.PackList(ref buffer, statModifiers);
+        }
+
+        public void Unpack(ref ReadOnlySpan<byte> buffer) {
+            resourceData = Packer.UnpackList<ResourceData>(ref buffer);
+            statModifiers = Packer.UnpackList<StatAndValue>(ref buffer);
+        }
+
+        #endregion IPacker
+        
+        #region IRegistryEntry
+        
+        public uint Hash => SmartPackerRegistry.GetHash(GetType());
+        
+        #endregion IRegistryEntry
+        
+        #region ISmartPacker
+        
+        public ISmartPacker CreateNewInstance() => new BehaviourData();
+
+        #endregion ISmartPacker
+
+        #region ToString
+        
+        public override string ToString() {
+            var sb = new StringBuilder(64);
+            sb.Append("BehaviourData [resources: ").Append(ResourceCount)
+              .Append(", modifiers: ").Append(ModifierCount)
+              .Append(", packed: ").Append(PackedSize).Append(" B]");
+
+            if (resourceData != null) {
+                for (var i = 0; i < resourceData.Count; i++)
+                    sb.Append("\n    res[").Append(i).Append("] ").Append(resourceData[i]);
+            }
+
+            if (statModifiers == null) return sb.ToString();
+
+            {
+                for (var i = 0; i < statModifiers.Count; i++)
+                    sb.Append("\n    mod[").Append(i).Append("] ").Append(statModifiers[i]);
+            }
+
+            return sb.ToString();
+        }
+
+        #endregion ToString
+        
+        #region Queries
+        
+        private const int CountPrefixBytes = sizeof(int);                   // length prefix per list
+        private const int ResourceBytes = sizeof(uint) + 3 * sizeof(float); // statHash + max + min + current
+        private const int ModifierBytes = sizeof(uint) + sizeof(float);     // statHash + amount
+
+        /// <summary>
+        /// Number of resource entries.
+        /// </summary>
+        public int ResourceCount => resourceData?.Count ?? 0;
+
+        /// <summary>
+        /// Number of stat modifier entries.
+        /// </summary>
+        public int ModifierCount => statModifiers?.Count ?? 0;
+
+        /// <summary>
+        /// Total number of packed entries across both lists.
+        /// </summary>
+        public int Count => ResourceCount + ModifierCount;
+
+        /// <summary>
+        /// True when there is no data to pack on either list.
+        /// </summary>
+        public bool IsEmpty => Count == 0;
+
+        /// <summary>
+        /// Exact size in bytes this struct will occupy once packed, computed without allocating. Mirrors the
+        /// layout written by <see cref="Pack"/>; keep the two in sync if either list's element shape changes.
+        /// </summary>
+        public int PackedSize =>
+                CountPrefixBytes + ResourceCount * ResourceBytes +
+                CountPrefixBytes + ModifierCount * ModifierBytes;
+
+        #endregion Queries
     }
 }
