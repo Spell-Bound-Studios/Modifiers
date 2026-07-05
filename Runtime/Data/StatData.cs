eb81c37e8b876409c9ba51dd9139e300c87cb811
Judsin <judsin1349@gmail.com>
2026-07-05 14:50:32 -0500

    stat data first full draft

--- Runtime/Data/StatData.cs
+++ Runtime/Data/StatData.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2026 Spellbound Studio Inc.
+// Copyright 2026 Spellbound Studio Inc.
 
 using System;
 using System.Collections.Generic;
@@ -17,10 +17,10 @@ namespace Spellbound.Modifiers {
     [PackerId("stat_data")]
     public struct StatData : IPackerObjectData {
         public List<ResourceData> resourceData; // Stat max, min, current
-        public List<StatAndValue> statModifiers; // Affix and Traits
-        // Buffs
-        // Debuffs
-        
+        public List<RolledModifier> modifiers;
+        public List<TimedModifier> buffs;
+        public List<TimedModifier> debuffs;
+
         public static class Context {
             public const byte Silent = 0;
             public const byte ResourcesGained = 1;
@@ -28,6 +28,44 @@ namespace Spellbound.Modifiers {
             public const byte ResourcesUnchanged = 3;
             public const byte Died = 4;
         }
+
+        #region Hydration
+
+        public readonly void ApplyTo(Modifiable target, TimedModifierSet buffSet = null, TimedModifierSet debuffSet = null) {
+            if (modifiers != null) {
+                for (var i = 0; i < modifiers.Count; i++)
+                    modifiers[i].TryApplyTo(target);
+            }
+
+            if (buffSet != null && buffs != null) {
+                for (var i = 0; i < buffs.Count; i++)
+                    buffSet.Restore(buffs[i]);
+            }
+
+            if (debuffSet != null && debuffs != null) {
+                for (var i = 0; i < debuffs.Count; i++)
+                    debuffSet.Restore(debuffs[i]);
+            }
+        }
+
+        public static StatData Capture(
+                List<ResourceData> resources, List<RolledModifier> applied,
+                TimedModifierSet buffSet = null, TimedModifierSet debuffSet = null) =>
+                new() {
+                    resourceData = resources,
+                    modifiers = applied != null ? new List<RolledModifier>(applied) : null,
+                    buffs = Snapshot(buffSet),
+                    debuffs = Snapshot(debuffSet)
+                };
+
+        private static List<TimedModifier> Snapshot(TimedModifierSet set) {
+            if (set == null || set.Active.Count == 0)
+                return null;
+
+            return new List<TimedModifier>(set.Active);
+        }
+
+        #endregion Hydration
 
         #region IPackerObjectData
 
@@ -44,64 +82,68 @@ namespace Spellbound.Modifiers {
                 this.ResolveCallback(context, parent, instanceIndex, preset, surfaceIndex, transformData);
 
         #endregion IPackerObjectData
-        
-        
+
         #region IPacker
 
         public void Pack(ref Span<byte> buffer) {
             Packer.PackList(ref buffer, resourceData);
-            Packer.PackList(ref buffer, statModifiers);
+            Packer.PackList(ref buffer, modifiers);
+            Packer.PackList(ref buffer, buffs);
+            Packer.PackList(ref buffer, debuffs);
         }
 
         public void Unpack(ref ReadOnlySpan<byte> buffer) {
             resourceData = Packer.UnpackList<ResourceData>(ref buffer);
-            statModifiers = Packer.UnpackList<StatAndValue>(ref buffer);
+            modifiers = Packer.UnpackList<RolledModifier>(ref buffer);
+            buffs = Packer.UnpackList<TimedModifier>(ref buffer);
+            debuffs = Packer.UnpackList<TimedModifier>(ref buffer);
         }
 
         #endregion IPacker
-        
+
         #region IRegistryEntry
-        
+
         public uint Hash => SmartPackerRegistry.GetHash(GetType());
-        
+
         #endregion IRegistryEntry
-        
+
         #region ISmartPacker
-        
+
         public ISmartPacker CreateNewInstance() => new StatData();
 
         #endregion ISmartPacker
 
         #region ToString
-        
+
         public override string ToString() {
             var sb = new StringBuilder(64);
             sb.Append("StatData [resources: ").Append(ResourceCount)
               .Append(", modifiers: ").Append(ModifierCount)
+              .Append(", buffs: ").Append(BuffCount)
+              .Append(", debuffs: ").Append(DebuffCount)
               .Append(", packed: ").Append(PackedSize).Append(" B]");
 
-            if (resourceData != null) {
-                for (var i = 0; i < resourceData.Count; i++)
-                    sb.Append("\n    res[").Append(i).Append("] ").Append(resourceData[i]);
-            }
+            for (var i = 0; i < ResourceCount; i++)
+                sb.Append("\n    res[").Append(i).Append("] ").Append(resourceData[i]);
 
-            if (statModifiers == null) return sb.ToString();
+            for (var i = 0; i < ModifierCount; i++)
+                sb.Append("\n    mod[").Append(i).Append("] ").Append(modifiers[i]);
 
-            {
-                for (var i = 0; i < statModifiers.Count; i++)
-                    sb.Append("\n    mod[").Append(i).Append("] ").Append(statModifiers[i]);
-            }
+            for (var i = 0; i < BuffCount; i++)
+                sb.Append("\n    buff[").Append(i).Append("] ").Append(buffs[i]);
+
+            for (var i = 0; i < DebuffCount; i++)
+                sb.Append("\n    debuff[").Append(i).Append("] ").Append(debuffs[i]);
 
             return sb.ToString();
         }
 
         #endregion ToString
-        
+
         #region Queries
-        
+
         private const int CountPrefixBytes = sizeof(int);                   // length prefix per list
         private const int ResourceBytes = sizeof(uint) + 3 * sizeof(float); // statHash + max + min + current
-        private const int ModifierBytes = sizeof(uint) + sizeof(float);     // statHash + amount
 
         /// <summary>
         /// Number of resource entries.
@@ -109,17 +151,21 @@ namespace Spellbound.Modifiers {
         public int ResourceCount => resourceData?.Count ?? 0;
 
         /// <summary>
-        /// Number of stat modifier entries.
+        /// Number of rolled modifier entries.
         /// </summary>
-        public int ModifierCount => statModifiers?.Count ?? 0;
+        public int ModifierCount => modifiers?.Count ?? 0;
+
+        public int BuffCount => buffs?.Count ?? 0;
+
+        public int DebuffCount => debuffs?.Count ?? 0;
 
         /// <summary>
-        /// Total number of packed entries across both lists.
+        /// Total number of packed entries across all lists.
         /// </summary>
-        public int Count => ResourceCount + ModifierCount;
+        public int Count => ResourceCount + ModifierCount + BuffCount + DebuffCount;
 
         /// <summary>
-        /// True when there is no data to pack on either list.
+        /// True when there is no data to pack on any list.
         /// </summary>
         public bool IsEmpty => Count == 0;
 
@@ -127,9 +173,25 @@ namespace Spellbound.Modifiers {
         /// Exact size in bytes this struct will occupy once packed, computed without allocating. Mirrors the
         /// layout written by <see cref="Pack"/>; keep the two in sync if either list's element shape changes.
         /// </summary>
-        public int PackedSize =>
-                CountPrefixBytes + ResourceCount * ResourceBytes +
-                CountPrefixBytes + ModifierCount * ModifierBytes;
+        public int PackedSize {
+            get {
+                var size = 4 * CountPrefixBytes + ResourceCount * ResourceBytes;
+
+                for (var i = 0; i < ModifierCount; i++)
+                    size += RolledModifierBytes(modifiers[i]);
+
+                for (var i = 0; i < BuffCount; i++)
+                    size += RolledModifierBytes(buffs[i].modifier) + 2 * sizeof(float);
+
+                for (var i = 0; i < DebuffCount; i++)
+                    size += RolledModifierBytes(debuffs[i].modifier) + 2 * sizeof(float);
+
+                return size;
+            }
+        }
+
+        private static int RolledModifierBytes(in RolledModifier modifier) =>
+                2 * sizeof(uint) + sizeof(int) + (modifier.values?.Length ?? 0) * sizeof(float);
 
         #endregion Queries
     }
